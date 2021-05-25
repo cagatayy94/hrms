ae3b8f8ed72ac9e0c43c77de97ebd0a4cf67c9a5
cagatayy94 <mustafa.cagatay.yilmaz@gmail.com>
2021-05-25 21:35:31 +0300

    Updated user class

--- src/main/java/spring/hrms/entities/concretes/User.java
+++ src/main/java/spring/hrms/entities/concretes/User.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 public class User {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
 

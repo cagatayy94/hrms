dfd6793a475d4a259f8acf1fdb5c176066b0fd4f
cagatayy94 <mustafa.cagatay.yilmaz@gmail.com>
2021-05-27 15:25:21 +0300

    Added position exist control

--- src/main/java/spring/hrms/business/concretes/PositionManager.java
+++ src/main/java/spring/hrms/business/concretes/PositionManager.java
@@ -3,10 +3,7 @@ package spring.hrms.business.concretes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import spring.hrms.business.abstracts.PositionService;
-import spring.hrms.core.utilities.results.DataResult;
-import spring.hrms.core.utilities.results.Result;
-import spring.hrms.core.utilities.results.SuccessDataResult;
-import spring.hrms.core.utilities.results.SuccessResult;
+import spring.hrms.core.utilities.results.*;
 import spring.hrms.dataAccess.abstracts.PositionDao;
 import spring.hrms.entities.concretes.Position;
 
@@ -29,7 +26,13 @@ public class PositionManager implements PositionService {
 
     @Override
     public Result add(Position position) {
-        this.positionDao.save(position);
-        return new SuccessResult("Position created successfully.");
+        List<Position> positionExist =  this.positionDao.findByName(position.getName());
+
+        if (!positionExist.isEmpty()){
+            return new ErrorResult("There is another position with this name.");
+        }else{
+            this.positionDao.save(position);
+            return new SuccessResult("Position created successfully.");
+        }
     }
 }
--- src/main/java/spring/hrms/dataAccess/abstracts/PositionDao.java
+++ src/main/java/spring/hrms/dataAccess/abstracts/PositionDao.java
@@ -3,5 +3,8 @@ package spring.hrms.dataAccess.abstracts;
 import org.springframework.data.jpa.repository.JpaRepository;
 import spring.hrms.entities.concretes.Position;
 
+import java.util.List;
+
 public interface PositionDao extends JpaRepository<Position, Integer> {
+    List<Position> findByName(String name);
 }
